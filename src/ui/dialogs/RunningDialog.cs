c4f94597d67e6b2ff15c65bb2bbed037242317d8
axel7083 <42176370+axel7083@users.noreply.github.com>
2024-03-16 15:04:49 +0100

    feat: allowing to config emulator executable to start container
    Signed-off-by: axel7083 <42176370+axel7083@users.noreply.github.com>

---
+++ src/ui/dialogs/RunningDialog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace SM64DSe.ui.dialogs
+{
+    public partial class RunningDialog : Form
+    {
+        private string executable;
+        public RunningDialog(string executable)
+        {
+            this.executable = executable;
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            RunProcessAsync();
+        }
+        
+        private void RunProcessAsync()
+        {
+            Process process = new Process();
+            process.StartInfo.FileName = this.executable;
+            process.StartInfo.Arguments = Program.m_ROMPath;
+            process.EnableRaisingEvents = true;
+            
+            process.Exited += (sender, e) =>
+            {
+                this.Close();
+                process.Dispose();
+            };
+
+            process.Start();
+        }
+    }
+}
